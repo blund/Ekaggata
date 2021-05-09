a43ee6526b01cc2089cad008e735890e8080f33c
Børge Lundsaunet <blund@blund.site>
2021-05-09 22:26:20 +0200

    add labels to assembler

--- assemble.py
+++ assemble.py
@@ -1,29 +1,66 @@
 
-print(' [ PYTHON KOMPILER ] \nKompilerer "test.lasm" til "build/test.xlasm"')
-
 inn = open("test.lasm", "r")
 ut  = open("build/test.xlasm", "w+")
-
 lines = inn.readlines()
 
+labels = {}
 
-while True:
-    if lines[-1] == "\n" or lines[-1] == "":
-        lines = lines[:-1]
-    else:
-        break
 
+labels["test"] = 2
+
+print(labels)
+
+
+
+
+# I første 'pass' vil vi bare fjerne tomme linjer og
+# erstatte labels med linjetall
+end = len(lines)
+i = 0
+while(i < end):
+
+    # Rydd bort tomme linjer
+    if lines[i] == '\n':
+        lines = lines[:i] + lines[i+1:]
+        print("BOM!")
+        end = len(lines)
+        continue
+    
+    has_colon = lines[i].find(":")
+
+    if(has_colon != -1):
+        label = lines[i][:-2]
+        labels[label] = i+1 # Legg til i labels (vil peke på neste linje), fjern semikolon
+        lines = lines[:i] + lines[i+1:]
+        print("BOM!")
+        end = len(lines)
+        continue
         
-for i in range(len(lines)):
+    # print(has_colon)
+    
+    i+=1
+
+# I andre 'pass' her vil vi erstatte labels med tilsvarende linje
+# Her setter vi også inn X-macro-markeringen
+    
+i = 0
+while(i < end):
+    if lines[i][0] == 'J':
+        for keys, val in labels.items():
+            lines[i] = lines[i].replace(keys, str(val))
+
     lines[i] = lines[i][:lines[i].find("//")] + " "
     lines[i].replace('\n', '')
     
-    if lines[i] == ' ':
-        continue
-    
-    if (lines[i] == lines[-1]):
-        ut.write("X(%s) \n" % lines[i])
+    if (i == end-1):
+        lines[i] = ("X(%s) \n" % lines[i])
     else:    
-        ut.write("X(%s) \\\n" % lines[i][0:-1])
+        lines[i] = ("X(%s) \\\n" % lines[i][0:-1])
 
-        
+    i+=1
+
+
+# Og til slutt outputter vi greier..
+for line in lines:
+    print(line)
+    ut.write(line)
